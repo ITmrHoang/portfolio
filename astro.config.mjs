3b4ba86cd501dacfc9bd8ff1ac570eb24840dd1f
HiMo <n.v.hoangcntt@gmail.com>
2026-04-02 22:58:49 +0700

    update plugin for js math mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,23 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://itmrhoang.github.io',
     base: '/portfolio',
-    integrations: [mdx(), tailwind(), react()],
+    integrations: [
+        mdx({
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex],
+        }),
+        tailwind(),
+        react()
+    ],
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
 });
